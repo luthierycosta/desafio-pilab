e05684d36e2e0091429447b3d103256e695670d3
Luthiery Costa <luthierycosta@gmail.com>
2021-10-09 08:03:13 -0300

    Primeiros usuários criados

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,7 +1,23 @@
-module.exports = {
-    create(req, res) {
+const Database = require("../db/config")
 
-        let id = 123
-        res.redirect(`/balanco/${id}`)
+module.exports = {
+    async create(req, res) {
+        const db = await Database()
+
+        const nome = req.body.novo_nome
+        const senha = req.body.nova_senha
+
+        await db.run(`
+            INSERT INTO usuario (
+                nome,
+                senha
+            ) VALUES (
+                '${nome}',
+                '${senha}'
+            )
+        `)
+
+        await db.close()
+        res.redirect(`/balanco/${nome.replace(' ','_')}`)
     }
 }
