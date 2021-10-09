055074659f797050aefeac3aae293f29532da0f1
Luthiery Costa <luthierycosta@gmail.com>
2021-10-09 00:34:51 -0300

    Primeira rota com a tela home funcionando

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const path = require('path')
 const route = require('./route')
 
 const server = express()
+
+server.set('view engine', 'ejs')
+server.set('views', path.join(__dirname, 'views'))
 
 server.use(route)
 
