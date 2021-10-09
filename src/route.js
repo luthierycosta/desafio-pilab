69df2f7fd542e2c52db1f9972f573faac8b53ce4
Luthiery Costa <luthierycosta@gmail.com>
2021-10-09 00:58:58 -0300

    CSS funcionando no Node

--- src/route.js
+++ src/route.js
@@ -3,5 +3,7 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index"))
+route.get('/cadastrar', (req, res) => res.render("cadastrar"))
+route.get('/balanco', (req, res) => res.render("balanco"))
 
 module.exports = route
